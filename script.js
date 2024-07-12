5d85aed256131264762b51a37e74b7440eb2077b
GiorgiBebia <139773975+GiorgiBebia@users.noreply.github.com>
2024-07-12 21:13:00 +0400

    Task Done

---
+++ script.js
@@ -0,0 +1,7 @@
+const h1 = document.querySelector(".h1");
+const paragraph = document.querySelector(".paragraph");
+
+function addText() {
+  h1.textContent = "Hello React 080524👋";
+  paragraph.textContent = "Goodbye";
+}
